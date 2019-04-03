7bfadf98ab81ec135340cf264fedcb5714031f8b
fernando24164 <fernando24164@gmail.com>
2019-04-03 22:04:45 +0200

    Add Server file and remove null check from tslint

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import express from 'express'
+import bodyParser = require('body-parser');
+import { getBlockchain, Block, generateNextBlock } from '.';
+import { getSockets, initPeerServer } from './peerConnection';
+
+const httpPort: number = parseInt(process.env.HTTP_PORT) || 3001
+const peersPort: number = parseInt(process.env.PEERS_PORT) || 6001
+
+const initHttpServer = (myHttpPort: number) => {
+    const app = express();
+    app.use(bodyParser.json());
+
+    app.get('/blocks', (req, res) => {
+        res.send(getBlockchain())
+    })
+
+    app.post('/mineBlock', (req, res) => {
+        let newBlock: Block = generateNextBlock(req.body.data);
+        res.send(newBlock)
+    })
+
+    app.get('/peers', (req, res) => {
+        res.send(getSockets().map((s: any): string => s.url))
+    })
+
+    app.post('/addPeer', (req, res) => {
+        res.status(201)
+        res.send('null')
+    })
+
+    app.listen(myHttpPort, () => {
+        console.log("Listening HTTP on port: " + myHttpPort);
+
+    })
+}
+
+initHttpServer(httpPort);
+initPeerServer(peersPort);
